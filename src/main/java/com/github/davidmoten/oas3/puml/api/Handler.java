d1dc2306fd9cb10d7e8ddb7ad21d3f0b64d26b38
Dave Moten <davidmoten@gmail.com>
2021-04-28 21:12:48 +1000

    update cf to get error integration response working (attempt)

--- src/main/java/com/github/davidmoten/oas3/puml/api/Handler.java
+++ src/main/java/com/github/davidmoten/oas3/puml/api/Handler.java
@@ -1,17 +1,18 @@
 package com.github.davidmoten.oas3.puml.api;
 
-import java.io.IOException;
-import java.io.UncheckedIOException;
 import java.util.Map;
+
+import org.apache.commons.lang3.exception.ExceptionUtils;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 import com.github.davidmoten.oas3.puml.Converter;
 
-import io.swagger.v3.core.util.Yaml;
 import net.sourceforge.plantuml.code.TranscoderSmart2;
 
 public final class Handler {
@@ -24,33 +25,47 @@ public final class Handler {
         // when a null body is passed `input.get("body-json") returns a LinkedHashMap
         // not a String (assumes encoded parameters perhaps?)
 
-        Object bodyJson = input.get("body-json");
-        if (!(bodyJson instanceof String)) {
-            throw new IllegalArgumentException("openapi deinition cannot be empty");
-        }
-        String body = (String) bodyJson;
-        if (body == null || body.trim().length() == 0) {
-            throw new IllegalArgumentException("openapi definition cannot be empty");
-        }
-        final String yaml;
-        if (body.trim().startsWith("{")) {
-            JsonNode jsonNodeTree;
-            try {
-                jsonNodeTree = new ObjectMapper().readTree(body);
-                yaml = new YAMLMapper().writeValueAsString(jsonNodeTree);
-            } catch (JsonProcessingException e) {
-                throw new RuntimeException(e);
-            }
-        } else {
-            yaml = body;
-        }
-        String puml = Converter.openApiToPuml(body);
         try {
+            Object bodyJson = input.get("body-json");
+            if (!(bodyJson instanceof String)) {
+                throw new IllegalArgumentException("openapi definition cannot be empty");
+            }
+            String body = (String) bodyJson;
+            if (body == null || body.trim().length() == 0) {
+                throw new IllegalArgumentException("openapi definition cannot be empty");
+            }
+            final String yaml;
+            if (body.trim().startsWith("{")) {
+                JsonNode jsonNodeTree;
+                try {
+                    jsonNodeTree = new ObjectMapper().readTree(body);
+                    yaml = new YAMLMapper().writeValueAsString(jsonNodeTree);
+                } catch (JsonProcessingException e) {
+                    throw new IllegalArgumentException(e.getMessage(), e);
+                }
+            } else {
+                yaml = body;
+            }
+            String puml = Converter.openApiToPuml(yaml);
             String encoded = new TranscoderSmart2().encode(puml);
             return "{\"puml\":\"" + escapeForJson(puml) + "\",\n" //
                     + "\"encoded\":\"" + escapeForJson(encoded) + "\"}";
-        } catch (IOException e) {
-            throw new UncheckedIOException(e);
+        } catch (Throwable e) {
+            // thrown error json created by lambda infrastructure is invalid in testing so building it ourselves:
+            ObjectMapper mapper = new ObjectMapper();
+            ObjectNode node = mapper.createObjectNode();
+            node.put("errorMessage", "Bad Request: " + String.valueOf(e.getMessage()));
+            node.put("errorType", e.getClass().getName());
+            ArrayNode array = node.putArray("stackTrace");
+            for (String line: ExceptionUtils.getStackTrace(e).split("\n")) {
+                array.add(line);
+            }
+            try {
+                return mapper.writeValueAsString(node);
+            } catch (JsonProcessingException e1) {
+                // shouldn't happen fingers crossed
+                throw new RuntimeException(e1);
+            }
         }
     }
 
