280024a7eebc22d657ef4404355e4cdda4971816
Dave Moten <davidmoten@gmail.com>
2023-02-07 16:13:57 +1100

    remove unused mode system property from deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 PARAMS="$1 $2 $3 $4 $5 $6 $7 $8"
-mvn clean package aws:property@prop aws:deployFileS3@file aws:deployCf@cf aws:deployRestApi@api aws:deployS3@s3 -Dmode=$MODE $PARAMS 
+mvn clean package aws:property@prop aws:deployFileS3@file aws:deployCf@cf aws:deployRestApi@api aws:deployS3@s3 $PARAMS 
